3eeb604e18753d6d556abb5308ae959d996b2551
Anchita Arora <anchita.arora@asos.com>
2020-09-16 22:15:42 +0100

    reverse a string kata

---
+++ Katas/Katas/ReverseAString.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+
+namespace Katas
+{
+    public class ReverseAStringTests
+    {
+        [TestCase("abc", "cba")]
+        [TestCase("a", "a")]
+        [TestCase(" ", " ")]
+        [TestCase("ABC!", "!CBA")]
+        public void GivenAStringThenReverseIt(string input, string expectedResult)
+        {
+            ReverseAString rev = new ReverseAString();
+            Assert.AreEqual(expectedResult, rev.Reverse(input));
+        }
+    }
+    public class ReverseAString
+    {
+        public string Reverse(string str)
+        {
+            string reverse = "";
+            for (int i = str.Length; i > 0; i--)
+            {
+                reverse = reverse + str[i - 1];
+            }
+
+            return reverse;
+        }
+    }
+}
